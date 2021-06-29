c3f3cfcee29bc462acf2ab8e6661f20a615f79f2
Kostyulin Ivan <ivan-kostyulin@yandex.ru>
2021-06-29 15:34:06 +0300

    fix: phone

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -11,7 +11,7 @@ function About() {
           <img className="education__img" src={education} alt="education"></img>
           <div className="education__info">
             <h2 className="education__title">Процесс обучения</h2>
-            <p className="education__subtitle">Вы получаете знания и&nbsp;используете единую современную и&nbsp;удобную<span className="education__subtitle_weight_bold"> образовательную платформу. </span>Все лекциии задания хранятся в&nbsp;одном месте.</p>
+            <p className="education__subtitle">Вы получаете знания и&nbsp;используете единую современную и&nbsp;удобную<span className="education__subtitle_weight_bold"> образовательную платформу. </span>Все лекции задания хранятся в&nbsp;одном месте.</p>
             <p className="education__subtitle">Записи онлайн-занятий и&nbsp;лекций доступны для&nbsp;студентов в&nbsp;любое время, вы&nbsp;всегда сможете к&nbsp;ним обратиться.</p>
             <p className="education__subtitle">Проверочные работы по&nbsp;итогам каждого семестра: студенты проходят тестирования, а&nbsp;также сдают зачеты и&nbsp;экзамены в&nbsp;режиме вебинара.</p>
           </div>
